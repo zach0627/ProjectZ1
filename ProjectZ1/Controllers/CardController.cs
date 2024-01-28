7bb2878e5ff4b53ed9b0635b7878188617c8babb
zach_liu <zach_liu@mayohr.com>
2024-01-29 01:43:40 +0800

    加入專案檔案。

---
+++ ProjectZ1/Controllers/CardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectZ1.Models;
+
+namespace ProjectZ1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController
+    {
+        private static List<Card> _cards = new List<Card>();
+
+        [HttpGet]
+        public List<Card> GetList()
+        {
+            return _cards;
+        }
+
+
+    }
+
+}
---
+++ ProjectZ1/Models/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectZ1.Models
+{
+    public class Card
+    {
+       public int Id { get; set; }
+       public string Name { get; set; }
+       public string Description { get; set; }
+    }
+}
